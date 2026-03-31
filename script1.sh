871de578585e2a94d1cee9435b9d3b6db103642d
Harshvardhand8999 <harshvardhand8999@gmail.com>
2026-03-31 22:41:30 +0530

    script1.sh

--- script1.sh
+++ script1.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Script 1: System Identity Report
-# Author: Prathamesh Kumar | Course: Open Source Software
+# Author: Harshvardhan | Course: Open Source Software
 
 # --- Variables ---
-STUDENT_NAME="Prathamesh Kumar"
+STUDENT_NAME="Harshvardhan"
 SOFTWARE_CHOICE="Visual Studio Code"
 
 # --- System Info ---
